3522c0ed230308d99b38b3e721a98249a1b6aab3
Chisel <chisel@chizography.net>
2016-04-15 17:26:49 +0100

    Improve behaviour with file:// development env

--- js/home.js
+++ js/home.js
@@ -1,20 +1,33 @@
 $(function () {
-  $(document).on('click', '.facebook-login', function(e) {
-    ref.authWithOAuthPopup("facebook", function(error, authData) {
-      if (authData === undefined) {
-        console.error(error);
-        return;
-      }
-      if (authData.uid !== null) {
-        if (authData.uid && window.location.pathname === "/") {
-            window.location.pathname = "/campaigns/";
+    if (window.location.protocol == "file:") {
+      ref.authAnonymously(function(error, authData) {
+        if (error) {
+            console.log("Authentication Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+            window.location.pathname =
+                window.location.pathname.replace(/index.html/, 'campaigns/index.html');
         }
-        else if (authData.uid && window.location.pathname !== null) {
-            window.location.pathname = window.location.pathname + "campaigns/";
+      });
+    }
+    else {
+    $(document).on('click', '.facebook-login', function(e) {
+        ref.authWithOAuthPopup("facebook", function(error, authData) {
+        if (authData === undefined) {
+            console.error(error);
+            return;
         }
-      }
-    }, {
-      scope: "email,public_profile"
+        if (authData.uid !== null) {
+            if (authData.uid && window.location.pathname === "/") {
+                window.location.pathname = "/campaigns/";
+            }
+            else if (authData.uid && window.location.pathname !== null) {
+                window.location.pathname = window.location.pathname + "campaigns/";
+            }
+        }
+        }, {
+        scope: "email,public_profile"
+        });
     });
-  });
+   }
 });
